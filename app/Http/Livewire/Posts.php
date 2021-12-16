7b548842122ab3e6a64357c9eb833209258822b3
seguna15 <seguna15@gmail.com>
2021-12-16 15:38:12 +0100

    nexted components restarted

--- app/Http/Livewire/Posts.php
+++ app/Http/Livewire/Posts.php
@@ -8,11 +8,39 @@ use Livewire\Component;
 class Posts extends Component
 {
    
+    public $title;
+    public $body;
+
+    protected $rules = [
+        'title' => 'required|min:6',
+        'body' => 'required|min:10',
+    ];
     
+    public function updated($field){
+        $this->validateOnly($field);
+    }
+
+    public function createPost(){
+        $this->validate();
+        Post::create([
+            'title' => $this->title,
+            'body' => $this->body
+        ]);
+        $this->clearForm();
+    }
+
+    public function clearForm(){
+        $this->title = '';
+        $this->body = '';
+    }
+
+    public function deletePost($id){
+        Post::findOrFail($id)->delete();
+    }
 
     public function render()
     {
-        return view('livewire.posts',[
+        return view('livewire.posts', [
             'posts' => Post::all()
         ]);
     }
--- resources/views/livewire/post-show.blade.php
+++
@@ -1,2 +0,0 @@
-<li>ID: {{$post->id}} {{ $post->title }}     <button wire:click="deletePost({{ $post->id }})" class="text-red-500">Delete</button>
-</li>
